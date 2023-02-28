4e849cefdb7d8379fc8a1e5ff4f4aec017f1b6d5
KRMadhusanka99 <Kkrmadhu1999@gmail.com>
2023-02-28 14:59:00 +0530

    allinone

---
+++ backend-app/src/test/java/com/EcommerceApp/backendapp/BackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EcommerceApp.backendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
