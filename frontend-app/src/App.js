2eaedaace22faa1acdd5a7f8f5cb8b75f4f7909b
Basic Lady Programmer <sutharsirajan@gmail.com>
2023-03-22 22:48:00 +0530

    Added SignUp and Login

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header/Header";
+import Login from './components/User/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Login/>
     </div>
   );
 }
