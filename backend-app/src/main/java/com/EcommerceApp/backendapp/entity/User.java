1e4542f9f9d0da4c3476296e1f058ab41471d9a5
KRMadhusanka99 <Kkrmadhu1999@gmail.com>
2023-03-25 02:03:45 +0530

    Add  Layered Arch

---
+++ backend-app/src/main/java/com/EcommerceApp/backendapp/entity/User.java
@@ -0,0 +1,59 @@
+package com.EcommerceApp.backendapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    @Column(name = "user_id",length = 10)
+    private int userId;
+
+    @Column(name = "user_name", length = 100, nullable = false)
+    private String userName;
+
+    @Column(name = "email", length = 50, nullable = false, unique = true)
+    private String email;
+
+    public User(int userId, String userName, String email) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
