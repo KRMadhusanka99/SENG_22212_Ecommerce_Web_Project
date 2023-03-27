05237b5b8feae6a5abdc73e3f8c9b9838da21b84
Basic Lady Programmer <sutharsirajan@gmail.com>
2023-03-27 16:23:20 +0530

    Added Header, Home, Product, Footer

--- frontend-app/src/index.js
+++ frontend-app/src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter} from 'react-router-dom';
+import { StateProvider } from './StateProvider';
+import reducer, { initialState } from './reducer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter>
+      <StateProvider initialState={initialState} reducer={reducer}>
+         <App />
+
+      </StateProvider>
+
+  </BrowserRouter>
   </React.StrictMode>
 );
 
